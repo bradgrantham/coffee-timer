f36c622d318ef58da40371ed0ae9c91139708a29
Brad Grantham <grantham@plunk.org>
2020-07-20 10:20:30 -0700

    add prototype coffee timer

--- coffeetimer.cpp
+++ coffeetimer.cpp
@@ -15,16 +15,76 @@ enum {
     STATE_FINISHED,
 } appState = STATE_DARK;
 
+// Play beepbeepbeep
+// Wait 1 seconds
+// repeat until timer expires
+enum {
+    FINISHED_BEEP,
+    FINISHED_QUIET,
+} finishedStateStep;
+
 int waitingTimer;
-constexpr int waitingToDarkSeconds = 10; // 5 * 60;
+int runningTimer;
+int finishedTimer;
+int beepSilenceTimer;
+int beepClip;
+constexpr int waitingToDarkDuration = 10; // 5 * 60;
+constexpr int button1TimerDuration = 5; // 1 * 60;
+constexpr int button2TimerDuration = 10; // 4 * 60;
+constexpr int finishedToWaitingDuration = 10; // 5 * 60;
+
+void DisplayTimeRemaining(int seconds)
+{
+    printf("%02d:%02d\n", seconds / 60, seconds % 60);
+}
+
+void EnterRunningState(int which)
+{
+    int seconds = (which == 0) ? button1TimerDuration : button2TimerDuration;
+    runningTimer = StartTimer(seconds);
+    DisplayTimeRemaining(seconds - 1);
+    appState = STATE_RUNNING;
+}
+
+void UpdateRunningScreen()
+{
+    DisplayTimeRemaining(GetTimerRemaining(runningTimer));
+}
 
 void EnterWaitingState()
 {
     if(debugStates) printf("EnterWaitingState\n");
     SetScreen(true);
     printf("draw 1m and 4m and smiley\n");
-    waitingTimer = StartTimer(waitingToDarkSeconds);
+    waitingTimer = StartTimer(waitingToDarkDuration);
     appState = STATE_WAITING;
+}
+
+uint8_t samples[8000]; // XXX fill
+
+void EnterFinishedState()
+{
+    if(debugStates) printf("EnterFinishedState\n");
+    appState = STATE_FINISHED;
+    finishedStateStep = FINISHED_BEEP;
+    beepClip = PlayClip(0, sizeof(samples));
+    finishedTimer = StartTimer(finishedToWaitingDuration);
+    printf("beep beep beep\n");
+}
+
+void UpdateFinishedScreen()
+{
+    if(finishedStateStep == FINISHED_BEEP) {
+        beepClip = -1;
+        beepSilenceTimer = StartTimer(1);
+        finishedStateStep = FINISHED_QUIET;
+        printf("...\n");
+    } else if(finishedStateStep == FINISHED_QUIET) {
+        beepSilenceTimer = -1;
+        beepClip = PlayClip(samples, sizeof(samples));
+        finishedStateStep = FINISHED_BEEP;
+        printf("beep beep beep\n");
+    }
 }
 
 void UpdateWaitingScreen()
@@ -55,6 +115,14 @@ int HandleEvent(const Event& e)
                     EnterWaitingState();
                     break;
                 case STATE_WAITING:
+                    CancelTimer(waitingTimer);
+                    if(e.data == BUTTON_1) {
+                        EnterRunningState(0);
+                    } else if(e.data == BUTTON_2) {
+                        EnterRunningState(1);
+                    } else {
+                        abort();
+                    }
                     // XXX enter timer
                     break;
                 case STATE_RUNNING:
@@ -64,7 +132,9 @@ int HandleEvent(const Event& e)
                     abort(); // implement me
                     break;
                 case STATE_FINISHED:
-                    abort(); // implement me
+                    CancelClip(beepClip);
+                    CancelTimer(finishedTimer);
+                    EnterWaitingState();
                     break;
             }
             break;
@@ -103,13 +173,24 @@ int HandleEvent(const Event& e)
                     EnterDarkState();
                     break;
                 case STATE_RUNNING:
-                    abort(); // implement me
+                    EnterFinishedState();
                     break;
                 case STATE_PAUSED:
                     abort(); // implement me
                     break;
                 case STATE_FINISHED:
-                    abort(); // implement me
+                    if(e.data == finishedTimer) {
+                        CancelTimer(beepSilenceTimer);
+                        beepSilenceTimer = -1;
+                        CancelClip(beepClip);
+                        beepClip = -1;
+                        EnterWaitingState();
+                    } else if(e.data == beepSilenceTimer) {
+                        beepSilenceTimer = -1;
+                        UpdateFinishedScreen();
+                    } else {
+                        abort();
+                    }
                     break;
             }
             break;
@@ -126,20 +207,21 @@ int HandleEvent(const Event& e)
                     UpdateWaitingScreen();
                     break;
                 case STATE_RUNNING:
-                    abort(); // implement me
+                    assert(e.data == runningTimer);
+                    UpdateRunningScreen();
                     break;
                 case STATE_PAUSED:
                     abort(); // implement me
                     break;
                 case STATE_FINISHED:
-                    abort(); // implement me
+                    // Nothing
                     break;
             }
             break;
         }
 
-        case PLAY_FINISHED: {
-            if(debugEvents) printf("PLAY_FINISHED %d\n", e.data);
+        case CLIP_FINISHED: {
+            if(debugEvents) printf("CLIP_FINISHED %d\n", e.data);
             switch(appState) {
                 case STATE_DARK:
                     // Won't Happen
@@ -154,7 +236,8 @@ int HandleEvent(const Event& e)
                     // Won't Happen
                     break;
                 case STATE_FINISHED:
-                    abort(); // implement me
+                    assert(finishedStateStep == FINISHED_BEEP);
+                    UpdateFinishedScreen();
                     break;
             }
             break;
