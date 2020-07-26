d8afba145ee48d4feaa514efb4c91ae448a5cd97
Brad Grantham <grantham@plunk.org>
2020-07-26 09:15:19 -0700

    fix draw test - add vertical justification

--- drawtest.cpp
+++ drawtest.cpp
@@ -26,15 +26,15 @@ int HandleEvent(const Event& e)
             switch((GetTimerRemaining(myTimer) / 10) % 3) {
                 case 0:
                     DrawRect(0, 0, 127, 127, Color(value, 0, 0));
-                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, Color(255, 255, 255), Color(value, 0, 0));
+                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, JUSTIFY_TOP, Color(255, 255, 255), Color(value, 0, 0));
                     break;
                 case 1:
                     DrawRect(0, 0, 127, 127, Color(0, value, 0));
-                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, Color(255, 255, 255), Color(0, value, 0));
+                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, JUSTIFY_TOP, Color(255, 255, 255), Color(0, value, 0));
                     break;
                 case 2:
                     DrawRect(0, 0, 127, 127, Color(0, 0, value));
-                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, Color(255, 255, 255), Color(0, 0, value));
+                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, JUSTIFY_TOP, Color(255, 255, 255), Color(0, 0, value));
                     break;
             }
             break;
@@ -52,15 +52,15 @@ int HandleEvent(const Event& e)
             switch((GetTimerRemaining(myTimer) / 10) % 3) {
                 case 0:
                     DrawRect(0, 0, 127, 127, Color(value, 0, 0));
-                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, Color(255, 255, 255), Color(value, 0, 0));
+                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, JUSTIFY_TOP, Color(255, 255, 255), Color(value, 0, 0));
                     break;
                 case 1:
                     DrawRect(0, 0, 127, 127, Color(0, value, 0));
-                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, Color(255, 255, 255), Color(0, value, 0));
+                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, JUSTIFY_TOP, Color(255, 255, 255), Color(0, value, 0));
                     break;
                 case 2:
                     DrawRect(0, 0, 127, 127, Color(0, 0, value));
-                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, Color(255, 255, 255), Color(0, 0, value));
+                    DrawText(32, 32, "hey there", JUSTIFY_LEFT, JUSTIFY_TOP, Color(255, 255, 255), Color(0, 0, value));
                     break;
             }
             break;
